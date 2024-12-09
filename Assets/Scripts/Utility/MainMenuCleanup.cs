7cb81ffb5a07c1a072e7b7b7616e990539bac261
Dario La Seta <dario.laseta@gmail.com>
2024-12-09 15:26:09 +0100

    added mainmenu cleanup

---
+++ Assets/Scripts/Utility/MainMenuCleanup.cs
@@ -0,0 +1,24 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class MainMenuCleanup : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (NetworkManager.Singleton != null)
+        {
+            Destroy(NetworkManager.Singleton.gameObject);
+        }
+
+        if (MultiplayerManager.Instance != null)
+        {
+            Destroy(MultiplayerManager.Instance.gameObject);
+        }
+
+        if (GameController.Instance != null)
+        {
+            Destroy(GameController.Instance.gameObject);
+        }
+    }
+}
